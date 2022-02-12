1203e2d371d03602affef5f49267862c4164a889
Jean Souza <jeanhrsouza@hotmail.com>
2022-02-12 12:31:25 -0300

    Ferramental de testes do Angular: nomeando teste

--- src/app/shared/services/unique-id/unique-id.service.spec.ts
+++ src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -1,6 +1,16 @@
+import { UniqueIdService } from './unique-id.service';
 
 //Por padrão o angular CLI utiliza um framework de teste para executar os testes, o jasmine
-describe('O artefato que queremos testar', () => {
-  it('Primeira condição que queremos testar', () => {});
+//Estrutura sugerida pelo jasmine ' blablabla SHOULD blabalbal WHEN blabalbla
+//pro tip: adicionar tralha na palavra para dizer que é um método. #generateUniqueIdWithPrefix
+  //A equipe do Angular faz a mesma coisa.
+describe('UniqueIdService', () => {
+  it('#generateUniqueIdWithPrefix should generate id when called with prefix', () => {
+    const service = new UniqueIdService(); // criando uma instância do servico
+    const id = service.generateUniqueIdWithPrefix('app');
+    expect(id).toContain('app-') //espero que o id passado contenha 'app-'
+  });
+
+
   it('Segunda condição que queremos testar', () => {});
 });
