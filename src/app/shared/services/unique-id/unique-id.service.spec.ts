ee67938fed5f0a11484422f309d71113b1513a03
Jean Souza <jeanhrsouza@hotmail.com>
2022-02-12 13:10:41 -0300

    Ferramental de testes do angular - removendo vicios

--- src/app/shared/services/unique-id/unique-id.service.spec.ts
+++ src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -4,11 +4,14 @@ import { UniqueIdService } from './unique-id.service';
 //Estrutura sugerida pelo jasmine ' blablabla SHOULD blabalbal WHEN blabalbla
 //pro tip: adicionar tralha na palavra para dizer que é um método. #generateUniqueIdWithPrefix
   //A equipe do Angular faz a mesma coisa.
-describe('UniqueIdService', () => {
-  it('#generateUniqueIdWithPrefix should generate id when called with prefix', () => {
+
+
+//Karma é um executor de testes criado pela prória equipe do Angular.
+describe(UniqueIdService.name, () => {
+  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} should generate id when called with prefix`, () => {
     const service = new UniqueIdService(); // criando uma instância do servico
     const id = service.generateUniqueIdWithPrefix('app');
-    expect(id).toContain('app-') //espero que o id passado contenha 'app-'
+    expect(id).toContain('app-')//espero que o id passado contenha 'app-'
   });
 
 
