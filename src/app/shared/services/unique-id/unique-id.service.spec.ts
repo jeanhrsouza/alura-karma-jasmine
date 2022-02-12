78d8a609a6b854d9b0d70a288dc71a6cbe930583
Jean Souza <jeanhrsouza@hotmail.com>
2022-02-12 11:01:11 -0300

    Ferramental de testes do angular: Jasmine, framwork de testes

---
+++ src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -0,0 +1,6 @@
+
+//Por padrão o angular CLI utiliza um framework de teste para executar os testes, o jasmine
+describe('O artefato que queremos testar', () => {
+  it('Primeira condição que queremos testar', () => {});
+  it('Segunda condição que queremos testar', () => {});
+});
