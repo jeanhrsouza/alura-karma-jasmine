b65961d3568b8e76571a58cbddecea0a5fa923b1
Jean Souza <jeanhrsouza@hotmail.com>
2022-02-12 14:32:53 -0300

    Precisao dos testes

--- src/app/shared/services/unique-id/unique-id.service.spec.ts
+++ src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -3,17 +3,13 @@ import { UniqueIdService } from './unique-id.service';
 //Por padrão o angular CLI utiliza um framework de teste para executar os testes, o jasmine
 //Estrutura sugerida pelo jasmine ' blablabla SHOULD blabalbal WHEN blabalbla
 //pro tip: adicionar tralha na palavra para dizer que é um método. #generateUniqueIdWithPrefix
-  //A equipe do Angular faz a mesma coisa.
-
+//A equipe do Angular faz a mesma coisa.
 
 //Karma é um executor de testes criado pela prória equipe do Angular.
 describe(UniqueIdService.name, () => {
   it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} should generate id when called with prefix`, () => {
     const service = new UniqueIdService(); // criando uma instância do servico
     const id = service.generateUniqueIdWithPrefix('app');
-    expect(id).toContain('app-')//espero que o id passado contenha 'app-'
+    expect(id.startsWith('app-')).toBeTrue();
   });
-
-
-  it('Segunda condição que queremos testar', () => {});
 });
