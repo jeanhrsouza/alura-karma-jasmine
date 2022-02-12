c0c75135bba84a29ff0868f552326393e603508d
Jean Souza <jeanhrsouza@hotmail.com>
2022-02-12 15:59:20 -0300

    LikeWidgetComponent

---
+++ src/app/shared/services/components/like-widget/like-widget.module.ts
@@ -0,0 +1,11 @@
+import { LikeWidgetComponent } from './like-widget.component';
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+@NgModule({
+  declarations: [LikeWidgetComponent],
+  imports: [CommonModule, FontAwesomeModule],
+  exports: [LikeWidgetComponent],
+})
+export class LikeWidgetModule {}
