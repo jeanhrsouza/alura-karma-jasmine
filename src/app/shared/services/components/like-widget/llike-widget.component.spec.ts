14a9f37aa039512ec6ef81218bf600828fd21bc3
Jean Souza <jeanhrsouza@hotmail.com>
2022-02-13 11:39:19 -0300

    Deteccao manual de mudanças

--- src/app/shared/services/components/like-widget/llike-widget.component.spec.ts
+++ src/app/shared/services/components/like-widget/llike-widget.component.spec.ts
@@ -1,4 +1,9 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { LikeWidgetModule } from './like-widget.module';
+import {
+  ComponentFixture,
+  ComponentFixtureAutoDetect,
+  TestBed,
+} from '@angular/core/testing';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { UniqueIdService } from './../../unique-id/unique-id.service';
 import { LikeWidgetComponent } from './like-widget.component';
@@ -9,9 +14,8 @@ describe(LikeWidgetComponent.name, () => {
   beforeEach(async () => {
     //TestBed, em suma, é uma solução para o programador não precisar ficar instanciando os componentes;
     await TestBed.configureTestingModule({
-      declarations: [LikeWidgetComponent],
-      providers: [UniqueIdService],
-      imports: [FontAwesomeModule],
+      imports: [LikeWidgetModule],
+      // providers: [{ provide: ComponentFixtureAutoDetect, useValue: true }], //provê para o component a detecção automática de detectChanges();
       //o compileComponents() vai aguardar a compilação do TS + template.
       //Desta forma, estamo blindando o código no sentido de tornar o código 'Feature Proof'
     }).compileComponents();
@@ -21,7 +25,22 @@ describe(LikeWidgetComponent.name, () => {
   });
 
   it('Should create component', () => {
-    const instance = fixture.componentInstance;
-    expect(instance).toBeTruthy();
+    const component = fixture.componentInstance;
+    expect(component).toBeTruthy();
+  });
+
+  it('Should auto generate ID when id input property is missing', () => {
+    //por padrão, o desenvolvedor é responsável por disparar a change detection.
+    const component = fixture.componentInstance;
+    fixture.detectChanges(); //detecta e passa pelo ngOnInit.
+    expect(component.id).toBeTruthy();
+  });
+
+  it('Should not generate ID when id input property is present', () => {
+    const component = fixture.componentInstance;
+    const someId = 'someId';
+    component.id = someId;
+    fixture.detectChanges();
+    expect(component.id).toBe(someId);
   });
 });
