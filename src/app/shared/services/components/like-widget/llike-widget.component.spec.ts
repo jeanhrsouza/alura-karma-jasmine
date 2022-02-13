6fad8941d24bed0165fb1f9d1d91c9b86d7a3ebe
Jean Souza <jeanhrsouza@hotmail.com>
2022-02-12 22:42:31 -0300

    Criando um instancia do componente

--- src/app/shared/services/components/like-widget/llike-widget.component.spec.ts
+++ src/app/shared/services/components/like-widget/llike-widget.component.spec.ts
@@ -1,17 +1,27 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { UniqueIdService } from './../../unique-id/unique-id.service';
 import { LikeWidgetComponent } from './like-widget.component';
 
 describe(LikeWidgetComponent.name, () => {
-  let component: LikeWidgetComponent = null;
+  let fixture: ComponentFixture<LikeWidgetComponent> = null;
 
   beforeEach(async () => {
     //TestBed, em suma, é uma solução para o programador não precisar ficar instanciando os componentes;
     await TestBed.configureTestingModule({
       declarations: [LikeWidgetComponent],
+      providers: [UniqueIdService],
+      imports: [FontAwesomeModule],
+      //o compileComponents() vai aguardar a compilação do TS + template.
+      //Desta forma, estamo blindando o código no sentido de tornar o código 'Feature Proof'
     }).compileComponents();
-    //o compileComponents() vai aguardar a compilação do TS + template.
-    //Desta forma, estamo blindando o código no sentido de tornar o código 'Feature Proof'
+
+    //Component Fixture
+    fixture = TestBed.createComponent(LikeWidgetComponent);
   });
 
-  it('', () => {});
+  it('Should create component', () => {
+    const instance = fixture.componentInstance;
+    expect(instance).toBeTruthy();
+  });
 });
